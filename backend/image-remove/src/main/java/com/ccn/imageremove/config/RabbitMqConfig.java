6c8c144e35afb7010c03e9177655bed443ecaf9e
NyeongC <cjfsud23@naver.com>
2026-06-04 22:30:46 +0900

    RabbitMQ Object 메시지 직렬화 오류 수정

--- backend/image-remove/src/main/java/com/ccn/imageremove/config/RabbitMqConfig.java
+++ backend/image-remove/src/main/java/com/ccn/imageremove/config/RabbitMqConfig.java
@@ -1,6 +1,10 @@
 package com.ccn.imageremove.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,5 +31,20 @@ public class RabbitMqConfig {
                 .bind(imageQueue())
                 .to(imageExchange())
                 .with(IMAGE_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory,
+            MessageConverter jsonMessageConverter
+    ) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
     }
 }
